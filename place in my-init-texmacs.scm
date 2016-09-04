852b75b9f71a0bd0327f60e87435a1f0f402c9dc
slowphil <coding.e.pjoyez@neverbox.com>
2016-09-04 13:13:55 +0200

    Update place in my-init-texmacs.scm

--- place in my-init-texmacs.scm
+++ place in my-init-texmacs.scm
@@ -7,23 +7,24 @@
 ;; to enable incoming connections
 
 (if (get-preference "zotero-server")
-  (let ((srv (client-start "localhost")))
-    (if (== srv -1) 
-        (begin (display "starting server\n")
+  (begin 
+;; Do (server-start) automatically at boot-up if preferences are set so
+;; to enable incoming connections
+    (import-from (server server-base)) ;; define tm-service for use below
+    (with srv (client-start "localhost")
+      (if (== srv -1) 
+        (begin (display "starting server\n")  ; no texmacs server was found : start it
                (server-start))
-        (begin (display "found local server\n")(client-stop srv) ;; other instance already running
-           (server-start)(server-stop)) ;; FIXME : stupid but otherwise server-related stuff is not defined, causing problems below
-        ))
-)
-
-(server-set-user-information "zotero" "zotero-remote-cite" "zotero" "" "no")
+        (begin (display "found local server\n")(client-stop srv)  ; a texmacs server is already started (in another instance, for example)
+        )))
+;; define a login for connecting to texmac's server
+    (server-set-user-information "zotero" "zotero-remote-cite" "zotero" "" "no")
 
 ;; define service that zotexmacs uses
 
-(tm-service (remote-cite key)
-  (if (list? key) 
-     (insert (append '(cite) key)) 
-     (insert `(cite ,key)))
-  (server-return envelope #t))
-
-
+    (tm-service (remote-cite key)
+      (if (list? key) 
+        (insert (append '(cite) key)) 
+        (insert `(cite ,key)))
+      (server-return envelope #t))
+  ))
