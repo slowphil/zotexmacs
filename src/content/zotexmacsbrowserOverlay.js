c0f1e52a31b831afdc459a250cca92ba32565632
pj <pj@spamgourmet.com>
2017-12-13 21:36:38 +0100

    Fixes for Zotero 5 (incompatible changes for Zotero 4)

--- src/content/zotexmacsbrowserOverlay.js
+++ src/content/zotexmacsbrowserOverlay.js
@@ -2,7 +2,7 @@
 //
 // MODULE      : Zotexmacs
 // DESCRIPTION : Extension enabling Zotero to interact with GNU TeXmacs
-// COPYRIGHT   : (C) 2016 Philippe Joyez
+// COPYRIGHT   : (C) 2016-2017 Philippe Joyez
 //
 // This software falls under the GNU general public license version 3 or later.
 // It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
@@ -28,7 +28,13 @@ Zotero.zotexmacs = {
 	
 
 
-	init : function() {	},
+	init : function() {	
+		if (Zotero.version.split('.')[0] < 5) {
+			var win = this.wm.getMostRecentWindow("navigator:browser");
+			win.alert('Warning: Zotemacs 0.5 is for Zotero 5 or higher. For Zotero 4, please install Zotexmacs 0.2 from https://github.com/slowphil/zotexmacs/releases/tag/v0.2');
+		  return false
+    }
+	},
 	
 	tm_connect : function (server, port) {
 	  this.tm_instream = Components.classes['@mozilla.org/scriptableinputstream;1'].createInstance(Components.interfaces.nsIScriptableInputStream);
@@ -82,25 +88,25 @@ Zotero.zotexmacs = {
 		this.tm_outstream.write(outData, outData.length);
 	},
     
-    tm_close : function () {
-	    // Close stream
-	    this.tm_outstream.close();
-	    this.tm_instream.close();
-	    this.tm_InputStream.close();
-	    // make sure we are actually disconnected before going on
-	    var start = new Date().getTime();
-	    for (var i = 0; i < 1e7; i++) { // wait for data or timeout after 500 ms
-			if (((new Date().getTime() - start) > 500 ) || !(this.tm_transport.isAlive())) {
-				this.tm_connected=false;
-				break;}
-	    }
-	    if (this.tm_transport.isAlive()) {console.log("NOT DISCONNECTED!!!!")}
-    },
-    
-    tm_write_and_read_answer : function (data) {
-	    this.tm_send(data);
-	    return this.tm_read();
-    },
+  tm_close : function () {
+    // Close stream
+    this.tm_outstream.close();
+    this.tm_instream.close();
+    this.tm_InputStream.close();
+    // make sure we are actually disconnected before going on
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) { // wait for data or timeout after 500 ms
+      if (((new Date().getTime() - start) > 500 ) || !(this.tm_transport.isAlive())) {
+        this.tm_connected=false;
+        break;}
+    }
+    if (this.tm_transport.isAlive()) {console.log("NOT DISCONNECTED!!!!")}
+  },
+  
+  tm_write_and_read_answer : function (data) {
+    this.tm_send(data);
+    return this.tm_read();
+  },
 
 	tm_login : function () {
 		this.tm_connect('localhost', 6561);
@@ -124,11 +130,11 @@ Zotero.zotexmacs = {
 		// syntax that we are using to communicate with texmacs:
 		// messages are of the form 
 		// (msg-id (service arg1 arg2 ...))
-		// where msg-id is a number, and service is the name of a tm-service that texmacs knows (that is defined somewhere in a scheme file)
+		// where msg-id is a number, and service is the name of a tm-service that texmacs knows (= it is defined somewhere in a scheme file)
 		// and that is responsible for handling the arguments
 		// args are optional and can be : boolean, string, list of string
 		// 
-		// (0 (remote-buffer-load "/tmp/QPC-P(E)15.tm"))
+		// (0 (remote-buffer-load "/tmp/myfile.tm"))
 		// (0 (remote-set-cursor-pos "(2 1 0 0 8 1)"))
 		// (0 (remote-login "zotero" "zotero"))
 		// (0 (remote-cite ("marcel" "gilbert" "henri")))
@@ -139,7 +145,6 @@ Zotero.zotexmacs = {
 		// and when receiving the message, texmacs expects first a string containing the number
 		// of bytes to read in the subsequent command (see tm_send())
 		
-		var win = this.wm.getMostRecentWindow("navigator:browser");
 		
 		// On windows , while probing instream.available() 
 		// (that is the method nsIScriptableInputStream.available)
@@ -159,6 +164,7 @@ Zotero.zotexmacs = {
 			if (this.fails<this.maxfails) {this.fails=0}
 			else {
 				// connect/login failed repeatedly
+    		var win = this.wm.getMostRecentWindow("navigator:browser");
 				win.alert("SERVER ERROR:\n" + "cannot connect with TeXmacs, or cannot login\n"+"try restarting TeXmacs or check config.");
 				this.tm_close();
 				return false
@@ -167,7 +173,7 @@ Zotero.zotexmacs = {
 			
 		// second, format args properly, send & get answer
 			
-		//console.log("typeof args :"+ typeof arg);
+		//Zotero.debug("ask_texmacs  typeof args :"+ typeof arg);
 		switch (true) {
 		  case (typeof arg === 'boolean') :
 				arg=(arg)?"#t":"#f";
@@ -183,7 +189,7 @@ Zotero.zotexmacs = {
 				arg = '';
 		        
 	      }
-		//console.log("args :"+arg);
+		//Zotero.debug("ask_texmacs args :"+arg);
 		repl = this.tm_write_and_read_answer("(0 ("+cmd+" "+arg+"))");
 		
 		//console.log("reply :"+repl);
@@ -200,79 +206,61 @@ Zotero.zotexmacs = {
 		else return str;
 	},
 
-	wait : function (delay_ms){
-		var start = new Date().getTime();
-		for (var i = 0; i < 1e7; i++) { // wait for data or timeout after delay
-			if ((new Date().getTime() - start) > delay_ms) {
-				break;}
-			}
-		},
-
-	item_key : function (zotero_item) {
-		try {
-			return Zotero.BetterBibTeX.keymanager.get(zotero_item, 'on-export').citekey
-		}
-		catch (x) {
-			// BetterBibTeX likely not installed : use zotero's default key scheme
-			// but no simple method to call to get the key
-			// we fake export and extract the key in it
-			var callback = function(obj, worked) {
-				text = obj.string;
-				};
-			var translation = new Zotero.Translate.Export;
-			translation.setTranslator("9cb70025-a888-4a29-a210-93ec52da40d4");//zotero's bibtex
-			translation.setHandler("done", callback);
-			
-			translation.setItems([zotero_item]);
-			translation.translate();
-			key = /@(?:.*){(.*),/.exec(text)[1];
-			return key;
-			}
-		},
-
-	tm_GetDoc : function() {
-		var res, fre, fname;
-		var win = this.wm.getMostRecentWindow("navigator:browser");
-		res = this.ask_texmacs("remote-get-buffer");
-		if (!res) {
-			win.alert("Could not contact TeXmacs ");
-			return null;
-		}
-
-		fname=res;
-		if (fname == null) {
-			win.alert("ERROR: tm_GetDoc: \n\n" + res);
-			return null;
-		}
-		return fname;
-	},
-
-
-	tm_GetPos : function() {
-		xy = this.ask_texmacs("remote-get-cursor-pos");
-		return xy;
-	},
-
 
 	checkAndCite : function() {
-		// export citation to texmacs
+		// insert "cite" tag in texmacs corresponding to the currently selected items
 		var win = this.wm.getMostRecentWindow("navigator:browser");
 		var zitems = win.ZoteroPane.getSelectedItems();
 		if (!zitems.length) {
 			win.alert("Please select at least one citation.");
 			return;
-		}
-		keys = new Array();
-		for ( var i = 0; i < zitems.length; i++) {
-			keys[i] = this.item_key(zitems[i])
-			};
-		this.fails=0;
+		};
+    this.item_keys(zitems).then(function(keys) {
+      this.fails=0;
+      Zotero.debug("before ask_texmacs");
+      var res = Zotero.zotexmacs.ask_texmacs("remote-cite", keys);
+//      Zotero.debug("ask_texmacs done");
+    });
 
-		res = this.ask_texmacs("remote-cite", keys);
-						
+	},
 
-	}
+  item_keys : Zotero.Promise.coroutine(function* (zitems) {	
+    var keys = [];
+    for ( var i = 0; i < zitems.length; i++) {
+      try {
+        keys[i] = Zotero.BetterBibTeX.KeyManager.get(zitems[i].id, 'on-export').citekey
+      }
+      catch (x) {
+        // Zotero.debug("catched :",x);
+        // BetterBibTeX likely not installed : use zotero's default key scheme
+        // but no simple method to call to get the key (maybe in Zotero 5.1?)
+        // we perform the bibtex export and extract the key in it
+        var translation = new Zotero.Translate.Export;
+        translation.setTranslator("9cb70025-a888-4a29-a210-93ec52da40d4");//zotero's bibtex
+        try {
+          var text = yield this._promiseTranslate(translation, [zitems[i]]);
+  //        Zotero.debug("promise returned :"+ text )
+        }
+        catch (e) {
+          Zotero.debug("translate: " + e);
+        }
+        keys[i] = /@(?:.*){(.*),/.exec(text)[1];
+        }
+      }
+    return keys
+  }),
+
+
+  _promiseTranslate : Zotero.Promise.coroutine(function* (translate, items) {
+    var text2;
+		translate.setHandler("done", function(translate, success) {
+	  	text2 = translate.string;
+			});
+		translate.setItems(items);
+		yield translate.translate();
+    return text2;
+  }),
 	
-	};
+};
 
 
